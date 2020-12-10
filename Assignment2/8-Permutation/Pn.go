642f0be7a934144a0689e1789a17c45f571dbf94
Vladimir Vodolazkiy <vloldemarus@gmail.com>
2020-12-10 10:39:45 +0300

    Permutation implemented

--- Assignment2/8-Permutation/Pn.go
+++ Assignment2/8-Permutation/Pn.go
@@ -2,61 +2,46 @@ package main
 
 import "fmt"
 
-type Vector struct {
-	Data []int
-}
-
-type Permutation struct {
-	Vectors []Vector
-}
-
-// Returns size of the vecotrs, stored in Permutation list
-func (p Permutation) size() int {
-	if len(p.Vectors) > 0 {
-		return len(p.Vectors[0].Data)
+func Permutation(data []int, i int, length int) {
+	if length == i {
+		// iteration completed, last element in array is reached
+		PrintSlice(data)
+	} else {
+		for j := i; j < length; j++ {
+			//replace current symbol with next
+			data[i], data[j] = data[j], data[i]
+			// calculate permutations for the tail of this state
+			Permutation(data, i+1, length)
+			// return back initial state
+			data[i], data[j] = data[j], data[i]
+		}
 	}
-	return 0
 }
 
-func (p Permutation) item(index int) Vector {
-	if index < 0 || index > (p.size()-1) {
-		v := new(Vector)
-		return *v // create and return empty vector
-	}
-	return p.Vectors[index]
+func PrintSlice(data []int) {
+	fmt.Println(data)
 }
 
-func (p Permutation) printAllItems() {
-	for i, v := range p.Vectors {
-		fmt.Printf("%2d : %v\n", i, v)
+func factorial(n int) int {
+	if n == 1 {
+		return 1
 	}
-	fmt.Println()
-}
-
-func PermutationCreate(arr []int) *Permutation {
-	len := len(arr)
-	if len <= 0 || len > 10 {
-		return nil
-	}
-	p := new(Permutation)
-
-	return p
+	return n * factorial(n-1)
 }
 
 func main() {
-	arraySize := []int{3, 4, 6, 8}
+	arraySize := []int{2, 3, 4}
 
 	for _, size := range arraySize {
 		fmt.Println("Permutation for arrry with length ", size)
+		fmt.Println("Amount of parmutations - ", factorial(size))
 		arr := make([]int, size)
 		// fill initial array
 		for i := 0; i < size; i++ {
 			arr[i] = i
 		}
-
-		p := PermutationCreate(arr)
-		p.printAllItems()
-
+		Permutation(arr[:], 0, len(arr))
+		fmt.Println()
 		fmt.Println()
 	}
 
